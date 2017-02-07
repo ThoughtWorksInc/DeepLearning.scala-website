82ad762f6e3b56a2a54b8121c0b5ad00774995c3
boydfd <boydfd@163.com>
2017-02-07 15:14:34 +0800

    [@rlin] #N/A fix bug : rename ci to commit.

--- deploy.sh
+++ deploy.sh
@@ -16,16 +16,16 @@ ls *.ipynb | sed "s/.ipynb/.html/g" | xargs make
 rm do_parse.sh
 rm Makefile
 git --git-dir=${git_path}/.git/ --work-tree=${git_path} commit -m "[auto] #001 auto generate the md from ipynb."
-git --git-dir=${git_path}/.git/ --work-tree=${git_path}  push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@${github_url}> bbb 2> bbb
+git --git-dir=${git_path}/.git/ --work-tree=${git_path}  push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@${github_url}> git_result 2> git_result
 exit_code=$?
-sed "s/${GITHUB_ACCESS_TOKEN}/**************/g" bbb
+sed "s/${GITHUB_ACCESS_TOKEN}/**************/g" git_result
 
 rm -rf deeplearning-website
 
 git add *.html 
-git ci -m "[auto] #001 generate html file."
-git push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@github.com/ThoughtWorksInc/DeepLearning.scala-website.git> bbb 2> bbb
+git commit -m "[auto] #001 generate html file."
+git push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@github.com/ThoughtWorksInc/DeepLearning.scala-website.git> git_result 2> git_result
 exit_code=$?
-sed "s/${GITHUB_ACCESS_TOKEN}/**************/g" bbb
-rm bbb
+sed "s/${GITHUB_ACCESS_TOKEN}/**************/g" git_result
+rm git_result
 exit ${exit_code}
