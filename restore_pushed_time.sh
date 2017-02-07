3a3700d9e8c95f1bfc6cb89e9c9981e31d32061c
auto <auto@thoughtworks.com>
2017-02-07 17:51:17 +0800

    [@boydfd] #N/A check time.

--- restore_pushed_time.sh
+++ restore_pushed_time.sh
@@ -3,6 +3,8 @@ do
     TIME=$(git log --pretty=format:%cd -n 1 --date=iso $FILE)
     TIME=$(python3 -c "from datetime import datetime;import sys;print(datetime.strptime(sys.argv[1], '%Y-%m-%d %H:%M:%S %z').strftime('%Y%m%d%H%M.%S'))
     " "${TIME}")
+    echo "${FILE}:${TIME}"
+
     touch -m -t $TIME $FILE
 done
 
