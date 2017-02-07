f6e6707352bae65b6ca102a384157e38a022ca91
boydfd <boydfd@163.com>
2017-02-07 14:51:55 +0800

    [auto] #001 generate html file.

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,16 @@ ls *.ipynb | sed "s/.ipynb/.html/g" | xargs make
 rm do_parse.sh
 rm Makefile
 git --git-dir=${git_path}/.git/ --work-tree=${git_path} commit -m "[auto] #001 auto generate the md from ipynb."
-git --git-dir=${git_path}/.git/ --work-tree=${git_path}  push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@${github_url}
+git --git-dir=${git_path}/.git/ --work-tree=${git_path}  push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@${github_url}> bbb 2> bbb
+exit_code=$?
+sed "s/${GITHUB_ACCESS_TOKEN}/**************/g" bbb
 
 rm -rf deeplearning-website
 
 git add *.html 
 git ci -am "[auto] #001 generate html file."
-git push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@github.com/ThoughtWorksInc/DeepLearning.scala-website.git
+git push https://tw-data-china-go-cd:${GITHUB_ACCESS_TOKEN}@github.com/ThoughtWorksInc/DeepLearning.scala-website.git> bbb 2> bbb
+exit_code=$?
+sed "s/${GITHUB_ACCESS_TOKEN}/**************/g" bbb
+rm bbb
+exit ${exit_code}
