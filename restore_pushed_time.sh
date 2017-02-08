668efdad8f3f33e8969e38e64c61c4de97809166
auto <auto@thoughtworks.com>
2017-02-08 18:07:13 +0800

    add support for directory level genegration.

--- restore_pushed_time.sh
+++ restore_pushed_time.sh
@@ -4,7 +4,6 @@ html_path=${2:-"demo"}
 for FILE in ${html_path}/*.html
 do
     TIME=$(git --git-dir=${git_path}/.git/ --work-tree=${git_path} log --pretty=format:%cd -n 1 --date=iso $FILE)
-    echo $TIME
     TIME=$(python3 -c "from dateutil import tz;utc_zone = tz.tzutc();import dateutil.parser;import sys;print(dateutil.parser.parse(sys.argv[1]).astimezone(utc_zone).strftime('%Y%m%d%H%M.%S'))
     " "${TIME}")
     echo "${FILE}:${TIME}"
@@ -18,13 +17,10 @@ cd $ipynbs_path
 for FILE in ${ipynbs_path}/*.ipynb
 do
     TIME=$(git log --pretty=format:%cd -n 1 --date=iso $FILE)
-    echo $TIME
     TIME=$(python3 -c "from dateutil import tz;utc_zone = tz.tzutc();import dateutil.parser;import sys;print(dateutil.parser.parse(sys.argv[1]).astimezone(utc_zone).strftime('%Y%m%d%H%M.%S'))
     " "${TIME}")
     echo "${FILE}:${TIME}"
-    echo \{
     touch -m -t $TIME $FILE
-    echo \}
 done
 
 
